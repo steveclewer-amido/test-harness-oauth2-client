6d4e7e6e6e0e806e02dad82cb8966088df6e9c73
Steve Clewer <steve.clewer@ensono.com>
2026-04-16 11:34:27 +0100

    Add example test class

---
+++ src/test/java/org/example/forgerock/ForgerockOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.forgerock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgerockOauth2ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
